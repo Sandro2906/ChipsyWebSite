f5a79adc1b8ec586c2c558bbcee665264c97e988
Sandro Gataric <132219558+Sandro2906@users.noreply.github.com>
2023-09-21 03:10:30 +0200

    Update VideoFullpage.jsx

--- frontend/src/components/VideoFullpage.jsx
+++ frontend/src/components/VideoFullpage.jsx
@@ -4,7 +4,7 @@ import Video from '../components/Video'
 
 const VideoFullpage = () => {
   return (
-    <div className='w-full h-auto my-auto mx-auto flex justify-center items-center md:py-14 overflow-hidden '>
+    <div className='w-full h-auto my-auto mx-auto flex justify-center items-center md:py-14 overflow-hidden z-0 '>
         <Video className="z-20" />
       <img src={bg} alt='BG' className='w-full h-full object-cover blur absolute p-20'/>
     </div>
