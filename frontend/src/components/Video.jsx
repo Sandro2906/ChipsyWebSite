aedf0c3e83a901c3646b6654931281170b237170
Sandro Gataric <132219558+Sandro2906@users.noreply.github.com>
2023-09-21 02:11:35 +0200

    Update Video.jsx

--- frontend/src/components/Video.jsx
+++ frontend/src/components/Video.jsx
@@ -31,10 +31,10 @@ const Video = () => {
 
   return (
     
-    <Carousel className="w-full h-auto z-20 mx-auto md:px-20 px-7 flex justify-center items-center" responsive={responsive}>
-      <iframe data-aos="zoom-in" className='md:w-full md:h-[400px] w-[350px] h-[180px] lg:h-[720px] lg:w-[1280px] z-20 object-cover' src="https://www.youtube.com/embed/kp9ptrS-e-A?si=M4cxXjdRnzCwVq1T" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      <iframe className='md:w-full md:h-[400px] w-[350px] h-[180px] z-20 lg:h-[720px] lg:w-[1280px]'  src="https://www.youtube.com/embed/IshAX7Yj1Us?si=AxCo5ghvn43YLFLG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      <iframe className='md:w-full md:h-[400px] w-[350px] h-[180px] z-20 lg:h-[720px] lg:w-[1280px]'  src="https://www.youtube.com/embed/3FsXmX93wGk?si=YZjebvmG94UCe6Ly" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <Carousel className="w-[700px] h-[500px] z-20 mx-auto" responsive={responsive}>
+      <iframe data-aos="zoom-in" className='md:w-full md:h-[400px] w-[350px] h-[180px] z-20 object-cover' src="https://www.youtube.com/embed/kp9ptrS-e-A?si=M4cxXjdRnzCwVq1T" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe className='md:w-full md:h-[400px] w-[350px] h-[180px] z-20'  src="https://www.youtube.com/embed/IshAX7Yj1Us?si=AxCo5ghvn43YLFLG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe className='md:w-full md:h-[400px] w-[350px] h-[180px] z-20'  src="https://www.youtube.com/embed/3FsXmX93wGk?si=YZjebvmG94UCe6Ly" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </Carousel>
   
   )
